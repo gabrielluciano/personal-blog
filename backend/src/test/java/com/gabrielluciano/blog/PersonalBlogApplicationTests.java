035bd9bd362dec2af15f8e6f188e3564e8195dec
Gabriel Luciano <gabriel-luciano@outlook.com>
2023-02-06 23:10:24 -0300

    Created spring boot backend project

---
+++ backend/src/test/java/com/gabrielluciano/blog/PersonalBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielluciano.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
