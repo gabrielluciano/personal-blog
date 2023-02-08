efb130e75842583d3322e3d4ae00b5932580df70
Gabriel Luciano <gabriel-luciano@outlook.com>
2023-02-08 20:52:02 -0300

    Added unique constraint for name and slug Category attributes

--- backend/src/main/java/com/gabrielluciano/blog/models/entities/Category.java
+++ backend/src/main/java/com/gabrielluciano/blog/models/entities/Category.java
@@ -23,9 +23,9 @@ public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = SEQUENCE_NAME)
     private Long id;
-    @Column(nullable = false, length = 50)
+    @Column(nullable = false, length = 50, unique = true)
     private String name;
-    @Column(nullable = false, length = 50)
+    @Column(nullable = false, length = 50, unique = true)
     private String slug;
     private String description;
 
