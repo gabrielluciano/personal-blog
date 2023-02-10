e819c3589e439dafef0a8918fb3c8d1ee99ac737
Gabriel Luciano <gabriel-luciano@outlook.com>
2023-02-10 00:21:14 -0300

    Added new methods to Tag repository

--- backend/src/main/java/com/gabrielluciano/blog/repositories/TagRepository.java
+++ backend/src/main/java/com/gabrielluciano/blog/repositories/TagRepository.java
@@ -1,7 +1,15 @@
 package com.gabrielluciano.blog.repositories;
 
 import com.gabrielluciano.blog.models.entities.Tag;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TagRepository extends CrudRepository<Tag, Long> {
+import java.util.Optional;
+
+public interface TagRepository extends JpaRepository<Tag, Long> {
+
+    Optional<Tag> findByIdAndDeletedAtNull(Long id);
+
+    Page<Tag> findAllByDeletedAtNull(Pageable pageable);
 }
