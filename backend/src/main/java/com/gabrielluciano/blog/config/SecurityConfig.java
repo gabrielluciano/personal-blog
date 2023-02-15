7e8d4e8a75ca6c7fe11789a88d1661b2106dba38
Gabriel Luciano <gabriel-luciano@outlook.com>
2023-02-15 20:59:36 -0300

    Created CustomUserDetailsService configuration class
    Changelog:
    - Created CustomUserDetailsService configuration class that implements UserDetailsService interface
    - Added the new CustomUserDetailsService as a bean in the SecurityConfig
    - Updated the UserRepository with the new findByEmail method

--- backend/src/main/java/com/gabrielluciano/blog/config/SecurityConfig.java
+++ backend/src/main/java/com/gabrielluciano/blog/config/SecurityConfig.java
@@ -1,16 +1,32 @@
 package com.gabrielluciano.blog.config;
 
+import com.gabrielluciano.blog.repositories.UserRepository;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebSecurity(debug = true)
 public class SecurityConfig {
+
+    private final UserRepository userRepository;
+
+    public SecurityConfig(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        return new CustomUserDetailsService(userRepository);
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -27,12 +43,15 @@ public class SecurityConfig {
                         })
                 ).and();
 
-        http.authorizeHttpRequests(requests -> requests
-                .requestMatchers(HttpMethod.GET, "/posts/**").permitAll()
-                .requestMatchers(HttpMethod.GET, "/categories/**").permitAll()
-                .requestMatchers(HttpMethod.GET, "/tags/**").permitAll()
-                .anyRequest().authenticated());
+        http
+                .authorizeHttpRequests(requests -> requests
+                        .requestMatchers(HttpMethod.GET, "/posts/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/categories/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/tags/**").permitAll()
+                        .anyRequest().authenticated()
+                );
 
+        http.httpBasic();
         return http.build();
     }
 
--- backend/src/main/java/com/gabrielluciano/blog/repositories/UserRepository.java
+++ backend/src/main/java/com/gabrielluciano/blog/repositories/UserRepository.java
@@ -3,5 +3,10 @@ package com.gabrielluciano.blog.repositories;
 import com.gabrielluciano.blog.models.entities.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
 }
