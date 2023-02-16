d1c7228ac6ae46a2259fb58034acdf7aaa4ba674
Gabriel Luciano <gabriel-luciano@outlook.com>
2023-02-15 21:32:30 -0300

    Removed unused imports from SecurityConfig

--- backend/src/main/java/com/gabrielluciano/blog/config/SecurityConfig.java
+++ backend/src/main/java/com/gabrielluciano/blog/config/SecurityConfig.java
@@ -7,10 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
