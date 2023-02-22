80b92ba5ce1516fd49d348bfc55774ddf17f08cd
Gabriel Luciano <gabriel-luciano@outlook.com>
2023-02-22 20:34:50 -0300

    Created AbstractPersistentObject class that provides uuid for entities
    Changelog:
    - AbstractPersistentObject is an abstract class that adds an uuid field for the entites that extends it
    - AbstractPersistentObject also provides an safe implementation for equals and hashCode based on the UUID
    - AbstractPersistentObject implements the PersistentObject interface
    - The UUIDGenerator class is used to generate the random UUIDs

---
+++ backend/src/main/java/com/gabrielluciano/blog/models/PersistentObject.java
@@ -0,0 +1,9 @@
+package com.gabrielluciano.blog.models;
+
+public interface PersistentObject {
+
+    String getUUID();
+
+    void setUUID(String UUID);
+
+}
---
+++ backend/src/main/java/com/gabrielluciano/blog/models/entities/AbstractPersistentObject.java
@@ -0,0 +1,47 @@
+package com.gabrielluciano.blog.models.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.gabrielluciano.blog.models.PersistentObject;
+import com.gabrielluciano.blog.util.UUIDGenerator;
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+
+import java.util.Objects;
+
+@MappedSuperclass
+public abstract class AbstractPersistentObject implements PersistentObject {
+
+    @Column(length = 32)
+    @JsonIgnore
+    private String UUID = UUIDGenerator.createUUID();
+
+    @Override
+    public String getUUID() {
+        return UUID;
+    }
+
+    @Override
+    public void setUUID(String UUID) {
+        this.UUID = UUID;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AbstractPersistentObject that = (AbstractPersistentObject) o;
+        return UUID.equals(that.UUID);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(UUID);
+    }
+
+    @Override
+    public String toString() {
+        return "AbstractPersistentObject{" +
+                "UUID='" + UUID + '\'' +
+                '}';
+    }
+}
