d4c3377bc3e1e259b8e00d3a5cbc189d3c583e28
Gabriel Luciano <gabriel-luciano@outlook.com>
2023-04-01 18:06:54 -0300

    Added builder to Tag entity

--- backend/src/main/java/com/gabrielluciano/blog/models/Tag.java
+++ backend/src/main/java/com/gabrielluciano/blog/models/Tag.java
@@ -6,7 +6,10 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
@@ -17,6 +20,9 @@ import lombok.Setter;
 )
 @Getter
 @Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Tag extends AbstractPersistentObject {
 
     public static final String SEQUENCE_NAME = "SEQUENCE_TAG";
