b56549c98fa34b8c5d6da25b6ce3862a9084d222
Gabriel Luciano <gabriel-luciano@outlook.com>
2023-04-05 16:41:21 -0300

    Refactored createValidTag method on TagCreator

--- backend/src/test/java/com/gabrielluciano/blog/util/TagCreator.java
+++ backend/src/test/java/com/gabrielluciano/blog/util/TagCreator.java
@@ -4,20 +4,17 @@ import com.gabrielluciano.blog.models.Tag;
 
 public class TagCreator {
 
+    private static final Tag tag = Tag.builder()
+            .id(1L)
+            .name("News")
+            .description("Get the latest news")
+            .slug("news")
+            .build();
+
     private TagCreator() {
     }
 
     public static Tag createValidTag() {
-        Tag tag = Tag.builder()
-                .id(1L)
-                .name("News")
-                .description("Get the latest news")
-                .slug("news")
-                .build();
-
-        tag.setUuid("uuid"); // Overrides the random uuid generated when tag is created
-
         return tag;
     }
-
 }
