86be62bb65cd08fb48bec439628f19763c4f83ae
Gabriel Luciano <gabriel-luciano@outlook.com>
2023-04-11 14:48:41 -0300

    Added DirtiesContext annotaion and refactored methods in TagControllerIT
    Changelog:
    - Added DirtiesContext annotation in TagControllerIT to allow testcontext to be cleared before each test
    - Refactored methods in TagControllerIT
    - Created custom logback configuration for tests

--- backend/src/test/java/com/gabrielluciano/blog/integration/TagControllerIT.java
+++ backend/src/test/java/com/gabrielluciano/blog/integration/TagControllerIT.java
@@ -4,7 +4,6 @@ import com.gabrielluciano.blog.dto.tag.TagResponse;
 import com.gabrielluciano.blog.models.Tag;
 import com.gabrielluciano.blog.repositories.TagRepository;
 import com.gabrielluciano.blog.util.TagCreator;
-import com.gabrielluciano.blog.util.TagResponseCreator;
 import com.gabrielluciano.blog.wrappers.RestPageImpl;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,11 +15,13 @@ import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureTestDatabase
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class TagControllerIT {
 
     @Autowired
@@ -32,8 +33,7 @@ class TagControllerIT {
     @Test
     @DisplayName("list returns page of tag responses when successful")
     void list_ReturnsPageOfTagResponses_WhenSuccessful() {
-        TagResponse expectedTagResponse = TagResponseCreator.createValidTagResponse();
-        tagRepository.save(TagCreator.createTagToBeSaved());
+        Tag savedTag = tagRepository.save(TagCreator.createTagToBeSaved());
 
         ResponseEntity<RestPageImpl<TagResponse>> responseEntity = restTemplate.exchange("/tags", HttpMethod.GET,
                 null, new ParameterizedTypeReference<>() {});
@@ -49,15 +49,19 @@ class TagControllerIT {
                 .isNotEmpty()
                 .hasSize(1);
 
-        assertThat(responseEntity.getBody().getContent().get(0))
-                .isNotNull()
-                .isEqualTo(expectedTagResponse);
+        assertThat(responseEntity.getBody().getContent().get(0)).isNotNull();
+
+        assertThat(responseEntity.getBody().getContent().get(0).getId())
+                .isEqualTo(savedTag.getId());
+
+        assertThat(responseEntity.getBody().getContent().get(0).getName())
+                .isEqualTo(savedTag.getName());
     }
 
     @Test
     @DisplayName("list returns empty page of tag responses when no tag is found")
     void list_ReturnsEmptyPageOfTagResponses_WhenNoTagIsFound() {
-        ResponseEntity<RestPageImpl<Tag>> responseEntity = restTemplate.exchange("/tags", HttpMethod.GET,
+        ResponseEntity<RestPageImpl<TagResponse>> responseEntity = restTemplate.exchange("/tags", HttpMethod.GET,
                 null, new ParameterizedTypeReference<>() {});
 
         assertThat(responseEntity).isNotNull();
@@ -74,18 +78,21 @@ class TagControllerIT {
     @Test
     @DisplayName("findById returns tag response when successful")
     void findById_ReturnsTagResponse_WhenSuccessful() {
-        TagResponse expectedTagResponse = TagResponseCreator.createValidTagResponse();
-        Tag expectedTag = tagRepository.save(TagCreator.createTagToBeSaved());
+        Tag savedTag = tagRepository.save(TagCreator.createTagToBeSaved());
 
         ResponseEntity<TagResponse> responseEntity = restTemplate
-                .getForEntity("/tags/{id}", TagResponse.class, expectedTag.getId());
+                .getForEntity("/tags/{id}", TagResponse.class, savedTag.getId());
 
         assertThat(responseEntity).isNotNull();
 
         assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
 
-        assertThat(responseEntity.getBody())
-                .isNotNull()
-                .isEqualTo(expectedTagResponse);
+        assertThat(responseEntity.getBody()).isNotNull();
+
+        assertThat(responseEntity.getBody().getId())
+                .isEqualTo(savedTag.getId());
+
+        assertThat(responseEntity.getBody().getName())
+                .isEqualTo(savedTag.getName());
     }
 }
