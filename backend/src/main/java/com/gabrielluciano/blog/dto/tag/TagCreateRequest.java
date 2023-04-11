a1bf474ea1a1ca5264492afc67e4019105c6816c
Gabriel Luciano <gabriel-luciano@outlook.com>
2023-04-10 21:22:12 -0300

    Updated lombok annotations in Tag DTOs

--- backend/src/main/java/com/gabrielluciano/blog/dto/tag/TagCreateRequest.java
+++ backend/src/main/java/com/gabrielluciano/blog/dto/tag/TagCreateRequest.java
@@ -2,12 +2,10 @@ package com.gabrielluciano.blog.dto.tag;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
