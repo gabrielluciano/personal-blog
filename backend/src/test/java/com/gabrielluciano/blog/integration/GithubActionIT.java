4eea412a8aa2ec91168cc61f454595d15062e435
Gabriel Luciano <gabriel-luciano@outlook.com>
2023-04-24 21:02:09 -0300

    Add temporary test class to test Github Actions test workflow

---
+++ backend/src/test/java/com/gabrielluciano/blog/integration/GithubActionIT.java
@@ -0,0 +1,15 @@
+package com.gabrielluciano.blog.integration;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Temporary class created to test Github Action maven test workflow
+ */
+class GithubActionIT {
+
+    @Test
+    void test() {
+        Assertions.assertThat(true).isFalse();
+    }
+}
