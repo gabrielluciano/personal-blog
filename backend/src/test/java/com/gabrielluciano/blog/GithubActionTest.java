1453973dee4690f94cc947738daa13e7b63500f2
Gabriel Luciano <gabriel-luciano@outlook.com>
2023-04-24 21:25:48 -0300

    Revert "Removes temporary GithubActionTest test class"
    This reverts commit 563bd4593d740af5fe88322a45b8ec57db507b37.

---
+++ backend/src/test/java/com/gabrielluciano/blog/GithubActionTest.java
@@ -0,0 +1,15 @@
+package com.gabrielluciano.blog;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Temporary class created to test Github Action maven test workflow
+ */
+class GithubActionTest {
+
+    @Test
+    void test() {
+        Assertions.assertThat(true).isFalse();
+    }
+}
