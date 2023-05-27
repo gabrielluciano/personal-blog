5707897134a76165ba89b306e4d6b2779fecb513
Gabriel Luciano <gabriel-luciano@outlook.com>
2023-05-27 12:32:14 -0300

    Add angular material lib and create header component
    Changelog:
    - Add angular material dependency
    - Update global styles.scss file with angular material theming and app global css
    - Create shared module
    - Create header component

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,10 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
+    imports: [RouterTestingModule, SharedModule],
     declarations: [AppComponent]
   }));
 
---
+++ frontend/src/app/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+
+import { HeaderComponent } from './header/header.component';
+
+@NgModule({
+  declarations: [
+    HeaderComponent
+  ],
+  imports: [],
+  exports: [
+    HeaderComponent
+  ],
+  providers: []
+})
+export class SharedModule { }
