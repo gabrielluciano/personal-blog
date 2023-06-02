de8bcfaf6c64a3a13568ae3d690d5cace2937fb2
Gabriel Luciano <gabriel-luciano@outlook.com>
2023-06-02 17:32:04 -0300

    Create post list component
    Changelog:
    - Create post-list and post-list-item components
    - Update routes

---
+++ frontend/src/app/posts/components/post-list-item/post-list-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-list-item',
+  templateUrl: './post-list-item.component.html',
+  styleUrls: ['./post-list-item.component.scss']
+})
+export class PostListItemComponent {
+
+}
--- frontend/src/app/posts/posts.module.ts
+++ frontend/src/app/posts/posts.module.ts
@@ -4,12 +4,16 @@ import { CommonModule } from '@angular/common';
 import { PostsRoutingModule } from './posts-routing.module';
 import { PostsComponent } from './pages/posts/posts.component';
 import { HeroComponent } from './components/hero/hero.component';
+import { PostListItemComponent } from './components/post-list-item/post-list-item.component';
+import { PostListComponent } from './components/post-list/post-list.component';
 
 
 @NgModule({
   declarations: [
     PostsComponent,
-    HeroComponent
+    HeroComponent,
+    PostListItemComponent,
+    PostListComponent
   ],
   imports: [
     CommonModule,
