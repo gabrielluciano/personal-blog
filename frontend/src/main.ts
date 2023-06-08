e6067cd6e57268efcc8d1a41704bf44c7d9aaf07
Gabriel Luciano <gabriel-luciano@outlook.com>
2023-06-08 11:55:34 -0300

    Remove unused import

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,5 @@
 /// <reference types="@angular/localize" />
 
-import { LOCALE_ID } from '@angular/core';
-
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
