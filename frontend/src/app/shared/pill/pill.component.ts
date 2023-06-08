910f911434eb6a63f10bf2e011b0ae1587398f63
Gabriel Luciano <gabriel-luciano@outlook.com>
2023-06-08 10:46:36 -0300

    Update pill component to receive path attribute

--- frontend/src/app/shared/pill/pill.component.ts
+++ frontend/src/app/shared/pill/pill.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-pill',
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class PillComponent {
 
+  @Input() path: string = "";
 }
