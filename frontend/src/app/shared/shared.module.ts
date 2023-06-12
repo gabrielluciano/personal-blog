e9660e23fbf413c63fc3d0a0884b25a81fe1cdda
Gabriel Luciano <gabriel-luciano@outlook.com>
2023-06-12 13:40:57 -0300

    Add responsiveness rules to components
    Changelog:
    - Create scss breakpoints variables
    - Add global box sizing configuration
    - Add media queries to components css

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -4,10 +4,11 @@ import { HeaderComponent } from './components/header/header.component';
 import { PillComponent } from './components/pill/pill.component';
 import { CommonModule } from '@angular/common';
 import { DateFormatPipe } from './pipes/date-format.pipe';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [HeaderComponent, PillComponent, DateFormatPipe],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [HeaderComponent, PillComponent, DateFormatPipe],
   providers: [],
 })
