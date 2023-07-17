3c290a39d1d2eda114e75e5a110648fe51c9cb5f
Gabriel Luciano <gabriel-luciano@outlook.com>
2023-07-17 10:48:45 -0300

    Update authGuard in auth.guard.ts to use the state
    Changelog:
    - Change authGuard function name to editorGuard in auth.guard.ts file
    - Refactor editorGuard to use the global AuthState instead of authService

--- frontend/src/app/posts/posts-routing.module.ts
+++ frontend/src/app/posts/posts-routing.module.ts
@@ -4,16 +4,16 @@ import { PostsHomeComponent } from './pages/posts-home/posts-home.component';
 import { PostsTagComponent } from './pages/posts-tag/posts-tag.component';
 import { PostComponent } from './pages/post/post.component';
 import { PostFormComponent } from './pages/post-form/post-form.component';
-import { authGuard } from '../shared/guards/auth.guard';
+import { editorGuard } from '../shared/guards/auth.guard';
 
 const routes: Routes = [
   { path: '', component: PostsHomeComponent },
-  { path: 'new', component: PostFormComponent, canActivate: [authGuard], data: { edit: false } },
+  { path: 'new', component: PostFormComponent, canActivate: [editorGuard], data: { edit: false } },
   { path: ':slug', component: PostComponent },
   {
     path: ':slug/edit',
     component: PostFormComponent,
-    canActivate: [authGuard],
+    canActivate: [editorGuard],
     data: { edit: true },
   },
   { path: 'tag/:tagId/:tagSlug', component: PostsTagComponent },
--- frontend/src/app/shared/guards/auth.guard.ts
+++ frontend/src/app/shared/guards/auth.guard.ts
@@ -1,14 +1,15 @@
 import { inject } from '@angular/core';
-import { Router } from '@angular/router';
-import { AuthService } from '../services/auth.service';
+import { CanActivateFn, Router } from '@angular/router';
+import { tap } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { AppState } from '../state/app.state';
+import { selectAuthIsEditor } from '../state/auth/auth.selectors';
 
-export const authGuard = async () => {
-  const authService = inject(AuthService);
+export const editorGuard: CanActivateFn = () => {
+  const store = inject(Store<AppState>);
   const router = inject(Router);
 
-  if (await authService.isEditor()) {
-    return true;
-  }
-  router.navigate(['/']);
-  return false;
+  return store
+    .select(selectAuthIsEditor)
+    .pipe(tap((isEditor) => isEditor || router.navigate(['/'])));
 };
