3c859d641125dab7a98023fad20316a4080f17f6
Gabriel Luciano <gabriel-luciano@outlook.com>
2023-08-01 11:52:07 -0300

    Fix contextLoad test by adding test profile
    Changelog:
    - Change h2 scope to test in pom.xml
    - Create application-test file
    - Update PersonalBlogApplicationTests file with ActiveProfiles annotation

--- backend/src/test/java/com/gabrielluciano/blog/PersonalBlogApplicationTests.java
+++ backend/src/test/java/com/gabrielluciano/blog/PersonalBlogApplicationTests.java
@@ -2,8 +2,10 @@ package com.gabrielluciano.blog;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PersonalBlogApplicationTests {
 
 	@Test
