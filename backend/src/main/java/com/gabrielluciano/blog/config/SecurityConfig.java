8daa2677e574f10a89d959c592e24395b3509ec3
Gabriel Luciano <gabriel-luciano@outlook.com>
2023-08-01 11:14:51 -0300

    Update request matchers in SecurityConfig and docker-compose file
    Changelog:
    - Update request matchers in SecurityConfig due to Spring Security changes related to CVE-2023-34035
    - Uncomment test in PersonalBlogApplicationTests file
    - Add database name environment variable in docker-compose.yml

--- backend/src/main/java/com/gabrielluciano/blog/config/SecurityConfig.java
+++ backend/src/main/java/com/gabrielluciano/blog/config/SecurityConfig.java
@@ -9,20 +9,27 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
+
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 @Configuration
 @RequiredArgsConstructor
+@EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
 
     private final JWTAuthenticationFilter jwtAuthenticationFilter;
+    private final HandlerMappingIntrospector introspector;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -44,10 +51,11 @@ public class SecurityConfig {
                 .exceptionHandling(conf -> conf.authenticationEntryPoint(new JWTAuthenticationEntryPoint()))
                 .addFilterAt(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(HttpMethod.GET, "/posts/**").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/tags/**").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/signup", "/login").permitAll()
-                        .requestMatchers("/users/**").hasRole(Role.ADMIN.name())
+                        .requestMatchers(antMatcher(HttpMethod.GET, "/posts/**")).permitAll()
+                        .requestMatchers(antMatcher(HttpMethod.GET, "/tags/**")).permitAll()
+                        .requestMatchers(antMatcher(HttpMethod.POST, "/signup")).permitAll()
+                        .requestMatchers(antMatcher(HttpMethod.POST, "/login")).permitAll()
+                        .requestMatchers(antMatcher("/users/**")).hasRole(Role.ADMIN.name())
                         .anyRequest().hasAnyRole(Role.EDITOR.name(), Role.ADMIN.name()))
                 .build();
     }
