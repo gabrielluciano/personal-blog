1da2f4a1ed4bd5cf2f9cc8207700b8fa1dd0dd8b
Gabriel Luciano <gabriel-luciano@outlook.com>
2023-08-21 15:15:26 -0300

    Create storage service
    Changelog:
    - Create StorageService and test file. This service is a wrapper for localStorage, this way we can provide an alternative implementation when platform is server (ssr)

---
+++ frontend/src/app/shared/services/storage.service.spec.ts
@@ -0,0 +1,132 @@
+import { TestBed } from '@angular/core/testing';
+
+import { StorageService } from './storage.service';
+import { PLATFORM_ID } from '@angular/core';
+
+describe('StorageService', () => {
+  let service: StorageService;
+
+  beforeEach(() => {
+    localStorage.clear();
+
+    TestBed.configureTestingModule({
+      providers: [{ provide: PLATFORM_ID, useFactory: () => 'browser' }],
+    });
+  });
+
+  it('should be created', () => {
+    service = TestBed.inject(StorageService);
+    expect(service).toBeTruthy();
+  });
+
+  it('should call localStorage clear when platform is browser', () => {
+    service = TestBed.inject(StorageService);
+    const localStorageClearSpy = spyOn(window.localStorage, 'clear').and.callThrough();
+
+    const [key, value] = ['some_key', 'some_value'];
+    localStorage.setItem(key, value);
+
+    service.clear();
+
+    expect(localStorageClearSpy).toHaveBeenCalledTimes(1);
+    expect(localStorage.getItem(key)).toBeNull();
+  });
+
+  it('should not call localStorage clear when platform is server', () => {
+    TestBed.overrideProvider(PLATFORM_ID, { useFactory: () => 'server' });
+    service = TestBed.inject(StorageService);
+    const localStorageClearSpy = spyOn(window.localStorage, 'clear');
+
+    service.clear();
+
+    expect(localStorageClearSpy).toHaveBeenCalledTimes(0);
+  });
+
+  it('should call localStorage getItem when platform is browser', () => {
+    service = TestBed.inject(StorageService);
+    const localStorageGetItemSpy = spyOn(window.localStorage, 'getItem').and.callThrough();
+
+    const [key, value] = ['some_key', 'some_value'];
+    localStorage.setItem(key, value);
+
+    const expectedValue = service.getItem(key);
+
+    expect(localStorageGetItemSpy).toHaveBeenCalledOnceWith(key);
+    expect(expectedValue).toBe(value);
+  });
+
+  it('should call localStorage getItem when platform is browser and get null return', () => {
+    service = TestBed.inject(StorageService);
+    const localStorageGetItemSpy = spyOn(window.localStorage, 'getItem').and.callThrough();
+
+    const key = 'some_key';
+
+    const expectedValue = service.getItem(key);
+
+    expect(localStorageGetItemSpy).toHaveBeenCalledOnceWith(key);
+    expect(expectedValue).toBeNull();
+  });
+
+  it('should not call localStorage getItem when platform is server and get null return', () => {
+    TestBed.overrideProvider(PLATFORM_ID, { useFactory: () => 'server' });
+    service = TestBed.inject(StorageService);
+    const localStorageGetItemSpy = spyOn(window.localStorage, 'getItem').and.callThrough();
+
+    const key = 'some_key';
+
+    const expectedValue = service.getItem(key);
+
+    expect(localStorageGetItemSpy).toHaveBeenCalledTimes(0);
+    expect(expectedValue).toBeNull();
+  });
+
+  it('should call localStorage removeItem when platform is browser', () => {
+    service = TestBed.inject(StorageService);
+    const localStorageRemoveItemSpy = spyOn(window.localStorage, 'removeItem').and.callThrough();
+
+    const key = 'some_key';
+    localStorage.setItem(key, 'some_value');
+
+    service.removeItem(key);
+
+    expect(localStorageRemoveItemSpy).toHaveBeenCalledOnceWith(key);
+    expect(localStorage.getItem(key)).toBeNull();
+  });
+
+  it('should not call localStorage removeItem when platform is server', () => {
+    TestBed.overrideProvider(PLATFORM_ID, { useFactory: () => 'server' });
+    service = TestBed.inject(StorageService);
+    const localStorageRemoveItemSpy = spyOn(window.localStorage, 'removeItem').and.callThrough();
+
+    const key = 'some_key';
+
+    service.removeItem(key);
+
+    expect(localStorageRemoveItemSpy).toHaveBeenCalledTimes(0);
+  });
+
+  it('should call localStorage setItem when platform is browser', () => {
+    service = TestBed.inject(StorageService);
+    const localStorageSetItemSpy = spyOn(window.localStorage, 'setItem').and.callThrough();
+
+    const [key, value] = ['some_key', 'some_value'];
+
+    service.setItem(key, value);
+
+    expect(localStorageSetItemSpy).toHaveBeenCalledOnceWith(key, value);
+    expect(localStorage.getItem(key)).toBe(value);
+  });
+
+  it('should not call localStorage setItem when platform is server', () => {
+    TestBed.overrideProvider(PLATFORM_ID, { useFactory: () => 'server' });
+    service = TestBed.inject(StorageService);
+    const localStorageSetItemSpy = spyOn(window.localStorage, 'setItem').and.callThrough();
+
+    const [key, value] = ['some_key', 'some_value'];
+
+    service.setItem(key, value);
+
+    expect(localStorageSetItemSpy).toHaveBeenCalledTimes(0);
+    expect(localStorage.getItem(key)).toBeNull();
+  });
+});
---
+++ frontend/src/app/shared/services/storage.service.ts
@@ -0,0 +1,35 @@
+import { isPlatformBrowser } from '@angular/common';
+import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class StorageService implements Partial<Storage> {
+  // eslint-disable-next-line
+  constructor(@Inject(PLATFORM_ID) private platformId: any) {}
+
+  clear(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.clear();
+    }
+  }
+
+  getItem(key: string): string | null {
+    if (isPlatformBrowser(this.platformId)) {
+      return localStorage.getItem(key);
+    }
+    return null;
+  }
+
+  removeItem(key: string): void {
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.removeItem(key);
+    }
+  }
+
+  setItem(key: string, value: string): void {
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.setItem(key, value);
+    }
+  }
+}
