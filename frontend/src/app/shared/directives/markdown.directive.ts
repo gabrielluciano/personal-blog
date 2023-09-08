046a9580c5418b37af4e2643c5e53a30bb80292f
Gabriel Luciano <gabriel-luciano@outlook.com>
2023-09-08 10:24:29 -0300

    Add new highlight.js languages and update code css
    Changelog:
    - Add new highlight.js languages to markdown directive
    - Update code style in post component scss file
    - Increase rows attribute in post-form textarea

--- frontend/src/app/shared/directives/markdown.directive.ts
+++ frontend/src/app/shared/directives/markdown.directive.ts
@@ -1,8 +1,24 @@
 import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
-import hljs from 'highlight.js/lib/core';
-import java from 'highlight.js/lib/languages/java';
 import { marked } from 'marked';
+import hljs from 'highlight.js/lib/core';
+
+import plaintext from 'highlight.js/lib/languages/plaintext';
+import xml from 'highlight.js/lib/languages/xml';
+import css from 'highlight.js/lib/languages/css';
+import scss from 'highlight.js/lib/languages/scss';
+import java from 'highlight.js/lib/languages/java';
+import javascript from 'highlight.js/lib/languages/javascript';
+import python from 'highlight.js/lib/languages/python';
+import typescript from 'highlight.js/lib/languages/typescript';
+import sql from 'highlight.js/lib/languages/sql';
+import pgsql from 'highlight.js/lib/languages/pgsql';
+import shell from 'highlight.js/lib/languages/shell';
+import bash from 'highlight.js/lib/languages/bash';
+import powershell from 'highlight.js/lib/languages/powershell';
+import json from 'highlight.js/lib/languages/json';
+import yaml from 'highlight.js/lib/languages/yaml';
+import dockerfile from 'highlight.js/lib/languages/dockerfile';
 
 @Directive({
   selector: '[appMarkdown]',
@@ -11,11 +27,30 @@ export class MarkdownDirective implements OnInit {
   @Input() appMarkdown = '';
 
   constructor(private el: ElementRef, private domSanitizer: DomSanitizer) {
-    hljs.registerLanguage('java', java);
+    this.registerLanguages();
   }
 
   ngOnInit(): void {
     this.el.nativeElement.innerHTML = this.domSanitizer.sanitize(1, marked.parse(this.appMarkdown));
     this.el.nativeElement.querySelectorAll('pre code').forEach(hljs.highlightElement);
+  }
+
+  private registerLanguages() {
+    hljs.registerLanguage('plaintext', plaintext);
+    hljs.registerLanguage('xml', xml);
+    hljs.registerLanguage('css', css);
+    hljs.registerLanguage('scss', scss);
+    hljs.registerLanguage('java', java);
+    hljs.registerLanguage('javascript', javascript);
+    hljs.registerLanguage('python', python);
+    hljs.registerLanguage('typescript', typescript);
+    hljs.registerLanguage('sql', sql);
+    hljs.registerLanguage('pgsql', pgsql);
+    hljs.registerLanguage('shell', shell);
+    hljs.registerLanguage('bash', bash);
+    hljs.registerLanguage('powershell', powershell);
+    hljs.registerLanguage('json', json);
+    hljs.registerLanguage('yaml', yaml);
+    hljs.registerLanguage('dockerfile', dockerfile);
   }
 }
