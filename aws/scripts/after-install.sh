21b6c06bab401b074677494414a2c624bb413d2c
Gabriel Luciano <gabriel-luciano@outlook.com>
2023-10-18 10:11:53 -0300

    Update backend deployment pipeline
    Changelog:
    - Add aws codedeploy aws/ folder and appspec file to bundle in backend-deploy workflow
    - Add instructions to copy application files in appspec.yml
    - Remove before-install script
    - Remove steps for copying bundle from s3 and extract it from after-install script, since codedeploy will already copy it
    - Moves docker image prune command from before-install to after-install script

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 set -xe
 
-cd /app/personal-blog
-
-aws s3 cp "s3://$PROD_S3_BUCKET/bundle.tar.gz" ./bundle.tar.gz
-
-tar -zxvf bundle.tar.gz
-
-rm -rf bundle.tar.gz
+# cleanup dangling images so we don't end up with full disk
+docker image prune --force
--- aws/scripts/before-install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -xe
-
-cd /app/personal-blog
-
-# cleanup dangling images so we don't end up with full disk
-docker image prune --force
