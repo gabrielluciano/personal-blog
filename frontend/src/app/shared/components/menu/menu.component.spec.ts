45c903c197095eac84acbb523c286ed2df4199af
Gabriel Luciano <gabriel-luciano@outlook.com>
2023-11-03 10:45:32 -0300

    Refactor header component and create menu component
    Changelog:
    - Refactor header component adding button to open menu
    - Create menu component
    - Update header tests moving some tests to menu component

---
+++ frontend/src/app/shared/components/menu/menu.component.spec.ts
@@ -0,0 +1,88 @@
+import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
+
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatIconModule } from '@angular/material/icon';
+
+import { MenuComponent } from './menu.component';
+import { AuthService } from '../../services/auth.service';
+import { AppState } from '../../state/app.state';
+import { initialState } from '../../state/auth/auth.reducer';
+import { logout } from '../../state/auth/auth.actions';
+
+describe('MenuComponent', () => {
+  let component: MenuComponent;
+  let fixture: ComponentFixture<MenuComponent>;
+  let authServiceSpy: jasmine.SpyObj<AuthService>;
+  let store: MockStore<AppState>;
+  let dialogSpy: jasmine.SpyObj<MatDialog>;
+  let snackBarSpy: jasmine.SpyObj<MatSnackBar>;
+  const initialAppState: AppState = { auth: initialState };
+
+  beforeEach(() => {
+    authServiceSpy = jasmine.createSpyObj<AuthService>('AuthService', ['logout']);
+    dialogSpy = jasmine.createSpyObj<MatDialog>('MatDialog', ['open']);
+    snackBarSpy = jasmine.createSpyObj<MatSnackBar>('MatSnackBar', ['openFromComponent']);
+
+    TestBed.configureTestingModule({
+      imports: [MatIconModule],
+      declarations: [MenuComponent],
+      providers: [
+        provideMockStore({ initialState: initialAppState }),
+        { provide: AuthService, useValue: authServiceSpy },
+        { provide: MatDialog, useValue: dialogSpy },
+        { provide: MatSnackBar, useValue: snackBarSpy },
+      ],
+    });
+    fixture = TestBed.createComponent(MenuComponent);
+    component = fixture.componentInstance;
+    store = TestBed.inject(MockStore);
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should call closeMenu when close menu button is clicked', fakeAsync(() => {
+    spyOn(component, 'closeMenu');
+    const button = fixture.debugElement.nativeElement.querySelector(
+      'button[aria-label="Fechar menu"]',
+    );
+
+    button.click();
+    tick();
+
+    expect(component.closeMenu).toHaveBeenCalled();
+  }));
+
+  it('should call mat dialog open method when login button is clicked', fakeAsync(() => {
+    const button = fixture.debugElement.nativeElement.querySelector('button[aria-label="Login"]');
+
+    button.click();
+    tick();
+
+    expect(dialogSpy.open).toHaveBeenCalled();
+  }));
+
+  it('should call logout method when user is authenticated and logout button is clicked', fakeAsync(() => {
+    const storeDispatchSpy = spyOn(store, 'dispatch');
+    store.setState({
+      auth: {
+        ...initialAppState.auth,
+        isAuthenticated: true,
+      },
+    });
+    tick();
+    fixture.detectChanges();
+
+    const button = fixture.debugElement.nativeElement.querySelector('button[aria-label="Logout"]');
+    button.click();
+    tick();
+
+    expect(authServiceSpy.logout).toHaveBeenCalled();
+    expect(storeDispatchSpy).toHaveBeenCalledWith(logout());
+    expect(snackBarSpy.openFromComponent).toHaveBeenCalled();
+  }));
+});
---
+++ frontend/src/app/shared/components/menu/menu.component.ts
@@ -0,0 +1,50 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+import { AppState } from '../../state/app.state';
+import { AuthService } from '../../services/auth.service';
+import { selectAuthIsAuthenticated } from '../../state/auth/auth.selectors';
+import { LoginFormComponent } from '../login-form/login-form.component';
+import { logout } from '../../state/auth/auth.actions';
+import { SUCCESS_LOGOUT_MSG } from 'src/app/i18n/pt/msg';
+import { showSnackBar } from '../snackbar/snackbar.component';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+})
+export class MenuComponent {
+  @Input() show: boolean = false;
+  @Output() whenClose: EventEmitter<void> = new EventEmitter();
+  authenticated$: Observable<boolean>;
+
+  constructor(
+    private store: Store<AppState>,
+    private authService: AuthService,
+    private dialog: MatDialog,
+    private _snackBar: MatSnackBar,
+  ) {
+    this.authenticated$ = store.select(selectAuthIsAuthenticated);
+  }
+
+  closeMenu() {
+    this.whenClose.emit();
+  }
+
+  openDialog() {
+    this.dialog.open(LoginFormComponent, {
+      width: '100%',
+      maxWidth: '560px',
+      panelClass: 'dialog',
+    });
+  }
+
+  logout() {
+    this.authService.logout();
+    this.store.dispatch(logout());
+    showSnackBar(this._snackBar, SUCCESS_LOGOUT_MSG, 'success');
+  }
+}
