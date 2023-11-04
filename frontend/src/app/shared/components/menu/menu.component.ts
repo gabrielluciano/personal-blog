ff66884cde37fa69b343818782c66996a6e8621a
Gabriel Luciano <gabriel-luciano@outlook.com>
2023-11-04 13:11:04 -0300

    Add style to post page and fix menu bug

--- frontend/src/app/shared/components/menu/menu.component.ts
+++ frontend/src/app/shared/components/menu/menu.component.ts
@@ -15,6 +15,7 @@ import { showSnackBar } from '../snackbar/snackbar.component';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss'],
 })
 export class MenuComponent {
   @Input() show: boolean = false;
